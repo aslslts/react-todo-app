0ee16eb7f45af78b069e221204774d46de09b6a2
Aslı Sulutaş <cemree.1402@gmail.com>
2022-12-26 15:51:20 +0300

    fixed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,6 +8,7 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     id: null,
     value: "",
   });
+
   const submitUpdate = (value) => {
     updateTodo(edit.id, value);
     setEdit({
